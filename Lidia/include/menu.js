d7dd3b5e73327c42c022e2b9160d98df06a27f98
Liferbis <liferbis@gmail.com>
2015-12-23 13:00:10 +0100

    e
    http://www.falconmasters.com/web-design/menu-desplegable-adaptable-dispositivos-moviles/

---
+++ Lidia/include/menu.js
@@ -0,0 +1,25 @@
+addEventListener('load', menuToggle, false);
+
+// var contador = 1;
+
+function menuToggle(){
+	if(document.getElementById('na').style.left == '-100%'){
+		document.getElementById('na').style.left = '0';
+	} else {
+		document.getElementById('na').style.left = '-100%';
+	}
+}
+
+
+ 
+
+ 
+// function main(){
+// 	$('.menu_bar').click(function(){
+// 		// $('nav').toggle(); 
+ 
+		
+ 
+// 	});
+ 
+// };
